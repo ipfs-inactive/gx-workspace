716c788c5975369623c1007ac38b6716437529ff
Lars Gierth <larsg@systemli.org>
2016-12-04 02:52:43 +0100

    Make update command work to some degree

--- main.go
+++ main.go
@@ -43,7 +43,7 @@ func main() {
 
 	app.Commands = []cli.Command{
 		PullCommand,
-		// UpdateCommand,
+		UpdateCommand,
 		// TestCommand,
 		ExecCommand,
 	}
@@ -63,28 +63,17 @@ var PullCommand = cli.Command{
 			return err
 		}
 
-		deps, err := EnumerateDependencies(&pkg)
+		pkgs, err := EnumerateDependencies(&pkg)
 		if err != nil {
 			return err
 		}
 
-		Log("pulling %d package repositories...", len(deps))
+		Log("pulling %d package repositories...", len(pkgs))
 
-		for h, dpkg := range deps {
-			dvcsimport := GxDvcsImport(dpkg)
+		for _, dpkg := range pkgs {
+			dvcsimport := GxDvcsImport(dpkg.Pkg)
 			if dvcsimport == "" {
-				return fmt.Errorf("package %s @ %s doesn't have gx.dvcsimport set", dpkg.Name, h)
-			}
-
-			// TODO: these won't go-get
-			//       can't load package: package github.com/gogo/protobuf: no buildable Go source files
-			if strings.HasPrefix(dvcsimport, "golang.org/x/") {
-				Log("warning: skipping %s (no buildable source files)", dvcsimport)
-				continue
-			}
-			if dvcsimport == "github.com/gogo/protobuf" {
-				Log("warning: skipping %s (no buildable source files)", dvcsimport)
-				continue
+				return fmt.Errorf("package %s doesn't have gx.dvcsimport set", dpkg.Pkg.Name)
 			}
 
 			// TODO: add option for passing -u, -v
@@ -93,7 +82,7 @@ var PullCommand = cli.Command{
 			cmd.Stdout = os.Stdout
 			cmd.Stderr = os.Stderr
 			if err = cmd.Run(); err != nil {
-				return fmt.Errorf("go get: %s", err)
+				Log("go get: %s", err)
 			}
 		}
 
@@ -118,22 +107,17 @@ var ExecCommand = cli.Command{
 			return err
 		}
 
-		deps, err := EnumerateDependencies(&pkg)
+		pkgs, err := EnumerateDependencies(&pkg)
 		if err != nil {
 			return err
 		}
 
-		Log("executing in %d package repositories...", len(deps))
+		Log("executing in %d package repositories...", len(pkgs))
 
-		for _, dpkg := range deps {
-			dir, err := PkgDir(dpkg)
-			if err != nil {
-				return err
-			}
-
+		for _, dpkg := range pkgs {
 			VLog("> sh -c '%s'", cmd)
 			cmd := exec.Command("sh", "-c", cmd)
-			cmd.Dir = dir
+			cmd.Dir = dpkg.Dir
 			cmd.Stdout = os.Stdout
 			cmd.Stderr = os.Stderr
 			if err = cmd.Run(); err != nil {
@@ -141,6 +125,159 @@ var ExecCommand = cli.Command{
 			}
 		}
 
+		Log("done.")
+
+		return nil
+	},
+}
+
+var UpdateCommand = cli.Command{
+	Name:  "update",
+	Usage: "updates the given package(s) and propagates the update through the dependency tree",
+	Action: func(c *cli.Context) error {
+		if len(c.Args()) == 0 {
+			return fmt.Errorf("update requires at least one package name")
+		}
+
+		var pkg gx.Package
+		err := gx.LoadPackageFile(&pkg, gx.PkgFileName)
+		if err != nil {
+			return err
+		}
+
+		pkgs, err := EnumerateDependencies(&pkg)
+		if err != nil {
+			return err
+		}
+
+		Log("updating in %d package repositories...", len(pkgs))
+
+		// We'll build a reversed dependency graph,
+		// starting at the given packages to-be-updated,
+		// and leading up to the package in the current working directory.
+		//
+		// We then flatten the graph, and that's our todo list of packages.
+
+		stack := map[string]bool{}
+		for _, p := range c.Args() {
+			stack[p] = true
+		}
+		preTodo := []string{}
+
+		todo := []string{}
+		done := []string{}
+
+		i := 0
+		for len(stack) > 0 {
+			if i++; i > 10000 {
+				return fmt.Errorf("infinite loop building dependency graph")
+			}
+			p, err := pkgFromStack(stack, pkgs)
+			if err != nil {
+				return err
+			}
+
+			for rp, _ := range pkgs {
+				for dp, _ := range pkgs[rp].Deps {
+					if dp == p {
+						stack[rp] = true
+					}
+				}
+			}
+			// this trick prepends to the array, which help with flattening the graph
+			preTodo = append([]string{p}, preTodo...)
+
+			delete(stack, p)
+		}
+
+		seen := map[string]bool{}
+		for _, p := range preTodo {
+			if _, s := seen[p]; s {
+				continue
+			}
+			seen[p] = true
+			todo = append([]string{p}, todo...)
+		}
+
+		// TODO: check that all todo pkgs gxdvcsimport dirs exist and are clean
+		// TODO: add -f flag for `git clean -fdx`
+
+		pm, err := makePM()
+		if err != nil {
+			return err
+		}
+
+		theCwd, err := os.Getwd()
+		if err != nil {
+			return err
+		}
+
+		for _, p := range todo {
+			pkg := pkgs[p].Pkg
+
+			err = os.Chdir(pkgs[p].Dir)
+			if err != nil {
+				return err
+			}
+
+			// 	mustRelease := false
+			// L:
+			// 	for _, dp := range pkgs {
+			// 		for _, dep := range dp.Pkg.Dependencies {
+			// 			if dep.Name == pkg.Name {
+			// 				mustRelease = true
+			// 				break L
+			// 			}
+			// 		}
+			// 	}
+
+			var h string
+			// if mustRelease {
+			err = updateVersion(pkg, "patch")
+			if err != nil {
+				return err
+			}
+
+			h, err = doPublish(pm, pkgs[p].Dir, pkg)
+			if err != nil {
+				return err
+			}
+
+			// if err = runRelease(pkg); err != nil {
+			// 	return err
+			// }
+
+			fmt.Printf("released %s %s @ %s\n", p, pkg.Version, h)
+			// }
+
+			err = os.Chdir(theCwd)
+			if err != nil {
+				return err
+			}
+
+			// update it within its dependants
+			for _, dp := range pkgs {
+				changed := false
+				for _, dep := range dp.Pkg.Dependencies {
+					if dep.Name == pkg.Name {
+						dep.Hash = h
+						dep.Version = pkg.Version
+						changed = true
+					}
+				}
+				if changed {
+					// VLog("gx.SavePackageFile(%s)", GxDvcsImport(dp.Pkg))
+					// err = gx.SavePackageFile(dp.Pkg, filepath.Join(dp.Dir, gx.PkgFileName))
+					// if err != nil {
+					// 	return err
+					// }
+				}
+			}
+
+			done = append(done, GxDvcsImport(pkg))
+		}
+
+		Log("\nupdated repos:\n\t%s\n", strings.Join(done, "\n\t"))
 		Log("done.")
 
 		return nil
--- util.go
+++ util.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"path/filepath"
 
+	semver "github.com/blang/semver"
 	gx "github.com/whyrusleeping/gx/gxutil"
 )
 
@@ -23,8 +24,14 @@ func PkgDir(pkg *gx.Package) (string, error) {
 	return filepath.Join(dir, GxDvcsImport(pkg)), nil
 }
 
-func EnumerateDependencies(pkg *gx.Package) (map[string]*gx.Package, error) {
-	deps := make(map[string]*gx.Package)
+type pkgInWorkspace struct {
+	Dir  string
+	Pkg  *gx.Package
+	Deps map[string]bool
+}
+
+func EnumerateDependencies(pkg *gx.Package) (map[string]pkgInWorkspace, error) {
+	deps := make(map[string]pkgInWorkspace)
 	err := enumerateDepsRec(pkg, deps)
 	if err != nil {
 		return nil, err
@@ -33,12 +40,22 @@ func EnumerateDependencies(pkg *gx.Package) (map[string]*gx.Package, error) {
 	return deps, nil
 }
 
-func enumerateDepsRec(pkg *gx.Package, deps map[string]*gx.Package) error {
-	for _, d := range pkg.Dependencies {
-		if _, ok := deps[d.Hash]; ok {
-			continue
-		}
+func enumerateDepsRec(pkg *gx.Package, deps map[string]pkgInWorkspace) error {
+	dir, err := PkgDir(pkg)
+	if err != nil {
+		return err
+	}
 
+	if _, found := deps[pkg.Name]; found {
+		return nil
+	}
+	deps[pkg.Name] = pkgInWorkspace{
+		Dir:  dir,
+		Pkg:  pkg,
+		Deps: map[string]bool{},
+	}
+
+	for _, d := range pkg.Dependencies {
 		var dpkg gx.Package
 		err := gx.LoadPackage(&dpkg, pkg.Language, d.Hash)
 		if err != nil {
@@ -48,12 +65,148 @@ func enumerateDepsRec(pkg *gx.Package, deps map[string]*gx.Package) error {
 			return err
 		}
 
-		deps[d.Hash] = &dpkg
+		deps[pkg.Name].Deps[dpkg.Name] = true
 
 		err = enumerateDepsRec(&dpkg, deps)
 		if err != nil {
 			return err
 		}
 	}
+	return nil
+}
+
+func makePM() (*gx.PM, error) {
+	cfg, err := gx.LoadConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	pm, err := gx.NewPM(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return pm, nil
+}
+
+// finds a package in `round` which doesn't depend on any other packages in `round`.
+func pkgFromStack(stack map[string]bool, pkgs map[string]pkgInWorkspace) (string, error) {
+	for p, _ := range stack {
+		_, found := pkgs[p]
+		if !found {
+			return "", fmt.Errorf("unknown package in this round: %s", p)
+		}
+
+		depsInThisRound := false
+		for tr, _ := range stack {
+			_, ok := pkgs[p].Deps[tr]
+			if ok {
+				depsInThisRound = true
+				break
+			}
+		}
+		if !depsInThisRound {
+			return p, nil
+		}
+	}
+
+	return "", fmt.Errorf("circular dependencies in %+v", stack)
+}
+
+func updateVersion(pkg *gx.Package, nver string) (outerr error) {
+	if nver == "" {
+		return fmt.Errorf("must specify version with non-zero length")
+	}
+
+	// defer func() {
+	// 	err := gx.SavePackageFile(pkg, gx.PkgFileName)
+	// 	if err != nil {
+	// 		outerr = err
+	// 	}
+	// }()
+
+	// if argument is a semver, set version to it
+	_, err := semver.Make(nver)
+	if err == nil {
+		pkg.Version = nver
+		return nil
+	}
+
+	v, err := semver.Make(pkg.Version)
+	if err != nil {
+		return err
+	}
+	switch nver {
+	case "major":
+		v.Major++
+		v.Minor = 0
+		v.Patch = 0
+		v.Pre = nil // reset prerelase info
+	case "minor":
+		v.Minor++
+		v.Patch = 0
+		v.Pre = nil
+	case "patch":
+		v.Patch++
+		v.Pre = nil
+	default:
+		if nver[0] == 'v' {
+			nver = nver[1:]
+		}
+		newver, err := semver.Make(nver)
+		if err != nil {
+			return err
+		}
+		v = newver
+	}
+
+	pkg.Version = v.String()
+
+	return nil
+}
+
+func doPublish(pm *gx.PM, dir string, pkg *gx.Package) (string, error) {
+	err := gx.TryRunHook("pre-publish", pkg.Language, pkg.SubtoolRequired)
+	if err != nil {
+		return "", err
+	}
+
+	hash, err := pm.PublishPackage(dir, &pkg.PackageBase)
+	if err != nil {
+		return "", fmt.Errorf("publish %s: %s", dir, err)
+	}
+
+	// err = writeLastPub(dir, pkg.Version, hash)
+	// if err != nil {
+	// 	return "", err
+	// }
+
+	err = gx.TryRunHook("post-publish", pkg.Language, pkg.SubtoolRequired, hash)
+	if err != nil {
+		return "", err
+	}
+
+	return hash, nil
+}
+
+func writeLastPub(dir, vers string, hash string) error {
+	err := os.MkdirAll(filepath.Join(dir, ".gx"), 0755)
+	if err != nil {
+		return err
+	}
+
+	fp := filepath.Join(dir, ".gx/lastpubver")
+	fi, err := os.Create(fp)
+	if err != nil {
+		return fmt.Errorf("create lastpubver: %s", err)
+	}
+
+	defer fi.Close()
+
+	_, err = fmt.Fprintf(fi, "%s: %s\n", vers, hash)
+	if err != nil {
+		return fmt.Errorf("failed to write version file: %s", err)
+	}
+
 	return nil
 }
